1996c31653c82bd770c7d63fccade3647ebb5b4a
Jin Chen <ac.chencho@gmail.com>
2025-12-17 00:13:00 -0500

    Notes

--- KrazyKarts/Source/KrazyKarts/GoKart.h
+++ KrazyKarts/Source/KrazyKarts/GoKart.h
@@ -47,14 +47,14 @@ private:
 
 	UPROPERTY(Replicated)
 	FVector Velocity;
-	
+
 	UPROPERTY(Replicated)
 	float Throttle;
 
 	UPROPERTY(Replicated)
 	float SteeringThrow;
 
-	UPROPERTY(ReplicatedUsing=OnRep_ReplicatedTransform)
+	UPROPERTY(ReplicatedUsing = OnRep_ReplicatedTransform)
 	FTransform ReplicatedTransform;
 
 	UFUNCTION()
@@ -70,4 +70,62 @@ private:
 	void UpdateLocationFromVelocity(float DeltaTime);
 	FVector GetAirResistance();
 	FVector GetRollingResistance();
+
+
+
+
+
+
+
+	/*
+	* Set the Move.Time = GetWorld()->TimeSeconds;
+	* Create a TArray
+	* Use the TArray inside the IsLocallyControlled block 
+	* Where the FGoKartMove is created
+	* Make a function extracting the Move instance.
+	* UnacknowledgedMoves.Add(Move); //This is the TArray
+	* Prune the Queue 
+	* Make a function that Clear the TArray -> param: FGoKartMove LastMove
+	* TArray<FGoKartMove> NewMoves;	 
+	*	for loop on the queue moves	
+	*		if(Move.Time > LastMove.Time)
+	*			NewMoves.Add(Move);
+	* Set the TArray to the NewMoves;
+	* 
+	* OnRep_ServerState -> Call Clear the TArray.
+	* if(!HasAuthority()) -> Add the move to the TArray
+	 */
+
+	/*
+	* Replay/Simulate all the AcknowledgedMoves
+	* OnRep_ServerState
+	* for loop SimulateMove(Move) on the TArray
+	* SimulateMove change to a const & param
+	*/
+
+	/*
+	* SimulateMove(Move) on the Tick move it inside the if check !HasAuthority()
+	* 
+	* Tick
+	* if(Role == ROLE_AutonomousProxy)
+	* {
+	*	Create the Move
+	*	Simulate
+	*	TArray add move
+	*	Server_SendMove
+	* }
+	* 
+	* // We are the server
+	* if(Role == ROLE_Authority && !IsLocallyControlled())
+	* {
+	*	Create move
+	*	Server_SendMove
+	* }
+	* 
+	* Client Prediction
+	* if(Role == ROLE_SimulatedProxy)
+	* {
+	*	SimulateMove(ServerState.LastMove);
+	* }
+	*/
 };
